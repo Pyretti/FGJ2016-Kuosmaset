b0db04241dbc8da434cef139cb7caabb5adcb897
SupremeSatan <lappi.jouko@gmail.com>
2016-01-30 16:24:59 +0200

    rge
    rfewrgew

--- Assets/Scripts/Misc/SlideButtonScript.cs
+++ Assets/Scripts/Misc/SlideButtonScript.cs
@@ -4,11 +4,13 @@ public class SlideButtonScript : MonoBehaviour {
     private RectTransform parentRect;
     private RectTransform pos;
     private GameObject player;
+    public Transform legalZone;
     
     void Start() {
         player = transform.parent.GetComponent<CreateButtons>().GetPlayer();
         parentRect = transform.parent.GetComponent<RectTransform>();
         pos = GetComponent<RectTransform>();                    //Minun positio
+        legalZone = GameObject.FindGameObjectWithTag("legalZone").transform;
     }
 
     void Update () {
@@ -16,14 +18,12 @@ public class SlideButtonScript : MonoBehaviour {
         if (pos.localPosition.x >= parentRect.rect.xMax) {
             player.GetComponent<BowingManScript>().hasFailed();
         }
-        
+
         //TODO: Korjaa oikein päin. Space väärässä = FALEEEE
-        if(pos.localPosition.x >= parentRect.rect.width / 3) {
-            if (Input.GetKey(KeyCode.Space)){
-                player.GetComponent<BowingManScript>().ChangeAnimator();
-                Destroy(gameObject);
-            }
+        if (pos.localPosition.x - pos.rect.width / 2 >= parentRect.rect.width / 3) {
+            transform.SetParent(legalZone);
         }
+
         pos.localPosition += new Vector3(Time.deltaTime * 150f, 0,0);
-	}
+    }
 }
